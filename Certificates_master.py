bfada5322c9c705050078c0cb002256e9adb2fea
Youssef El-Gammal <youssouf99amr@gmail.com>
2019-07-21 18:30:15 +0200

    Add files via upload

---
+++ Certificates_master.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageDraw, ImageFont
+import numpy
+
+font = ImageFont.truetype('your font file', size=140) #you can change the size
+color = 'rgb(0, 0, 0)' # black color
+
+image = Image.open('My template path')
+imageWidth = image.width
+imageHeight = image.height
+
+names = list()
+names= [file.rstrip('\n') for file in open('the names CSV file path')]
+
+for name in names:
+    nameSize = font.getsize(name)
+    image = Image.open('The name of the template along with its format')
+    draw = ImageDraw.Draw(image)
+
+    w, h = font.getsize(name)
+    pos = ((imageWidth - w)/2, (imageHeight - h)/2)
+    #draw the message on the background
+    draw.text(pos, name, fill=color, font=font)
+
+    # save the edited image
+    image.save('Certificate_' + name + '.png')
